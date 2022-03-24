b2eca691d4abfae987b8d792b861f74385d2393e
BrayanSV18 <brayanvenegas24@gmail.com>
2022-03-24 14:23:49 -0600

    Initialize and create vaiable file

---
+++ variables.js
@@ -0,0 +1,19 @@
+//Ways to define a variable
+
+//Var is globally defined. This can be a string or number.
+var variableName = "Brayan";
+
+//Let is block scoped. This can be a string or number.
+let variableName = 22;
+
+//Const is block scoped and can't be reassigned. This can be string or number.
+const variableName = "Persona";
+
+/*-------------------------------*/
+
+//We can declare the variable and initialize it afterwards (después). 
+
+//Declare
+var variableName;
+//Inintialize 
+variableName = "contenido"; 

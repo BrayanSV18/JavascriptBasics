c00347fc699595d429de410340a9fc54356dc9ba
BrayanSV18 <brayanvenegas24@gmail.com>
2022-03-25 12:12:25 -0600

    Scope function

---
+++ scope.js
@@ -0,0 +1,15 @@
+/*Scope Global and Local*/
+
+//Global variables are accessible from anywhere in the code.
+//Local variables are accesible only inside the function.
+var globalVariable = "Global";
+
+function functionName() {
+  var localVariable = "Local";
+
+  //Here it returns both varibles
+  return globalVariable + localVariable;
+}
+
+//Here only return the Local Variable
+functionName();

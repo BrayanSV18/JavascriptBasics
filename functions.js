eadda8159aef427d59c6285614bb0d72b799036e
BrayanSV18 <brayanvenegas24@gmail.com>
2022-03-25 08:54:26 -0600

    Syntax of Declarative and Imperative functions

---
+++ functions.js
@@ -0,0 +1,19 @@
+//Declarative and Imperative
+
+//Declarative
+function functionName() {
+  return 10;
+}
+//To call the function 
+functionName();
+
+//Imperative
+var functionName = function(a,b){
+  return a + b;
+}
+//To call the function
+functionName();
+
+/*-----------------*/
+//To storage the variable data 
+var result = a + b;

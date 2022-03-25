17db86fd1121924df6ff7d7bb94297cc56a4ca60
BrayanSV18 <brayanvenegas24@gmail.com>
2022-03-25 11:30:17 -0600

    Created and edited Hosting.js

---
+++ hoisting.js
@@ -0,0 +1,11 @@
+/*It means we can call a function and define it below (después)*/
+
+//Declarative
+functionName();
+
+function functionName(parameters) {
+  return 0;
+}
+
+//Imperative
+/*It doesn't work with the imperative function, It has to be declared before being called.*/
